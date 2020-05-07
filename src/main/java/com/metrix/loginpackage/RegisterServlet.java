ddc98f8a71bfd8dd51e85fe69bf56622f299b8b7
Anil Poudyal <anilrunjakote@gmail.com>
2020-05-07 20:43:00 +0545

    Removed Common Files Upload Dependency

--- src/main/java/com/metrix/loginpackage/RegisterServlet.java
+++ src/main/java/com/metrix/loginpackage/RegisterServlet.java
@@ -13,9 +13,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import org.apache.commons.fileupload.FileItem;
-import org.apache.commons.fileupload.disk.DiskFileItemFactory;
-import org.apache.commons.fileupload.servlet.ServletFileUpload;
 
 /**
  *
