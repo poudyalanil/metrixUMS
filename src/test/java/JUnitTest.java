e166fb75cffe4f9fb6b3bb5200cf5596d3d0155f
iamkhadka46 <58580576+iamkhadka46@users.noreply.github.com>
2020-05-25 15:23:18 +0545

    Added Forgot Password, Edit profile for User, Bug Fixes

--- src/test/java/JUnitTest.java
+++ src/test/java/JUnitTest.java
@@ -4,13 +4,21 @@
  * and open the template in the editor.
  */
 
+import com.metrix.loginpackage.ConnectionProvider;
 import com.metrix.loginpackage.User;
+import java.sql.Connection;
+import static com.metrix.loginpackage.ConnectionProvider.getConnection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import java.time.LocalDate;
+import CRUDuser.UserDAO;
+import com.metrix.loginpackage.UserDatabase;
 
 /**
  *
@@ -53,4 +61,109 @@ public class JUnitTest {
     user.setEmail("Rameshkc@gmail.com");
     assertEquals("Email","Rameshkc@gmail.com", user.getEmail());
     }
+    
+    @Test
+    public void testOpenConnection1( ) throws Exception{
+        System.out.println("openConnection");
+        Connection result = ConnectionProvider.getConnection();
+        assertEquals(result != null,true);
+    }
+    
+    @Test
+    public void testOpenConnection2( ) throws Exception{
+        System.out.println("openConnection");
+        Connection result = ConnectionProvider.getConnection();
+        assertNotNull(result);
+    }
+    
+    @Test
+    public void testOpenConnection3( ) throws Exception{
+        System.out.println("openConnection");
+        Connection result = ConnectionProvider.getConnection();
+        assertTrue(result !=null);
+    }
+   
+    @Test
+    public void testOpenConnection4( ) throws Exception{
+        System.out.println("openConnection");
+        Connection result = null;
+        assertNull(result);
+    }
+    
+    @Test
+    public void testOpenConnection5( ) throws Exception{
+        System.out.println("openConnection");
+        Connection result = ConnectionProvider.getConnection();
+        assertNotNull(result);
+    }
+    
+    @Test
+    public void testAddUserEmpty() throws Exception{
+        boolean ans = false;
+        boolean val;
+    
+        User user = new User("","","","","","",null);
+   
+        UserDatabase dao = new UserDatabase();
+        val = dao.saveUser(user);
+        assertEquals(ans,val);
+        
+     }
+    
+    @Test
+    public void testAddUserFirstName() throws Exception{
+        boolean ans = false;
+        boolean val;
+    
+        User user = new User("Ram","","","","","",null);
+   
+        UserDatabase dao = new UserDatabase();
+        val = dao.saveUser(user);
+        assertEquals(ans,val);
+        
+     }
+    
+    @Test
+    public void testAddUserAddress() throws Exception{
+        boolean ans = false;
+        boolean val;
+    
+        User user = new User("","","","KTM","","",null);
+   
+        UserDatabase dao = new UserDatabase();
+        val = dao.saveUser(user);
+        assertEquals(ans,val);
+        
+     }
+    
+    @Test
+    public void testAddUserMiddleName() throws Exception{
+        boolean ans = false;
+        boolean val;
+    
+        User user = new User("","Kumar","","","","",null);
+   
+        UserDatabase dao = new UserDatabase();
+        val = dao.saveUser(user);
+        assertEquals(ans,val);
+        
+     }
+    
+    @Test
+    public void testAddUserLastName() throws Exception{
+        boolean ans = false;
+        boolean val;
+    
+        User user = new User("","","KC","","","",null);
+   
+        UserDatabase dao = new UserDatabase();
+        val = dao.saveUser(user);
+        assertEquals(ans,val);
+        
+     }
+    
+    
+
+
+
 }
