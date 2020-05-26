f09405fbbebd367ddf713a379d40ec0b1bd51152
Anil Poudyal <anilrunjakote@gmail.com>
2020-05-26 09:17:01 +0545

    Test Cases

--- src/test/java/CRUDuser/UserDAOTest.java
+++ src/test/java/CRUDuser/UserDAOTest.java
@@ -6,7 +6,6 @@
 package CRUDuser;
 
 import com.metrix.loginpackage.User;
-import java.sql.Connection;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import java.time.LocalDate;
@@ -18,76 +17,135 @@ import org.junit.Before;
  */
 public class UserDAOTest {
 
-//    public LocalDate today = LocalDate.of(2020, 05, 10);
-//    UserDAO userObj = new UserDAO();
-//    
+    public LocalDate today = LocalDate.of(2020, 05, 10);
+   
+    
     public UserDAOTest() {
     
     }
-    
+    @Before
+    public void setUp() throws Exception{
+        
+    }
 
     @Test
     public void testAddUser() {
-//        User user = new User();
-//        user.setFirstName("Anil");
-//        user.setMiddleName("Parsad");
-//        user.setLastName("Poudyal");
-//        user.setAddress("Kathmandu");
-//        user.setEmail("anil@anill.com");
-//        user.setPassword("12345678");
-//        user.setJoinDate(today);
-//        userObj.addUser(user);
-//        
-//        assertEquals("Anil",user.getFirstName());
-//        
+        User user = new User();
+        UserDAO userObj = new UserDAO();
         
+        user.setFirstName("Anil");
+        user.setMiddleName("Parsad");
+        user.setLastName("Poudyal");
+        user.setAddress("Kathmandu");
+        user.setEmail("anil@anill.com");
+        user.setPassword("12345678");
+        user.setJoinDate(today);
+       
+        userObj.addUser(user);
+        String actual = "Anil";
+        String expected = user.getFirstName();
+        
+        if(actual.equals(expected)){
+            assertTrue(true);
+        }else{
+            assertTrue(false);
+        }      
     }
 
-    @Test
-    public void testGetAllUsers() {
-    }
-
+   
 
     @Test
     public void testGetSingleUser() {
+        UserDAO usersInfo = new UserDAO();
+        User user = usersInfo.getSingleUser(14);
         
-// User usr = userObj.getSingleUser(0);
-//          
-//assertEquals("Successfully fetched user","Anil",usr.getFirstName());
-//      
+        if(user !=null ){
+            assertTrue(true);
+        }else{
+            assertTrue(false);
+        }
+            
     }
 
     @Test
     public void testBlockUser() {
-//        User usr = userObj.getSingleUser(14);
-//        userObj.blockUser(14);
-//        int isBlock = usr.getUserStatus();
-//        assertEquals(0, isBlock);
+        UserDAO usersInfo = new UserDAO();
+        usersInfo.blockUser(14);
+        User user = usersInfo.getSingleUser(14);
+        
+        if(user.getUserStatus() ==0 ){
+            assertTrue(true);
+        }else{
+            assertTrue(false);
+        }
+        
+            
     }
 
     @Test
     public void testUnBlockUser() {
-//        User usr = userObj.getSingleUser(15);
-//        userObj.unBlockUser(15);
-//        int isBlock = usr.getUserStatus();
-//        assertEquals(0, isBlock);
+
+        UserDAO usersInfo = new UserDAO();
+        usersInfo.unBlockUser(14);
+        User user = usersInfo.getSingleUser(14);
+        
+        if(user.getUserStatus() ==1 ){
+            assertTrue(true);
+        }else{
+            assertTrue(false);
+        }
     }
 
     @Test
     public void testResetPassword() {
+        UserDAO usersInfo = new UserDAO();
+        usersInfo.resetPassword(14);
+        User user = usersInfo.getSingleUser(14);
         
+        if(user.getPassword().equals("password!!") ){
+            assertTrue(true);
+        }else{
+            assertTrue(false);
+        }
     }
 
     @Test
     public void testDeleteUser() {
+        UserDAO usersInfo = new UserDAO();
+        usersInfo.deleteUser(14);
+        User user = usersInfo.getSingleUser(14);
+        
+        if(user ==null ){
+            assertTrue(true);
+        }else{
+            assertTrue(false);
+        }
     }
 
     @Test
     public void testMakeAdmin() {
+        UserDAO usersInfo = new UserDAO();
+        usersInfo.makeAdmin(14);
+        User user = usersInfo.getSingleUser(14);
+        
+        if(user.getUserRole() ==1 ){
+            assertTrue(true);
+        }else{
+            assertTrue(false);
+        }
     }
 
     @Test
     public void testMakeNormalUser() {
+        UserDAO usersInfo = new UserDAO();
+        usersInfo.makeNormalUser(14);
+        User user = usersInfo.getSingleUser(14);
+        
+        if(user.getUserRole() ==0 ){
+            assertTrue(true);
+        }else{
+            assertTrue(false);
+        }
     }
     
 }
